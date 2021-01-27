9aacd6bccadee0198d5abd068d5a7578b2584a58
Jaeki-Kwon <worl1126@naver.com>
2021-01-27 11:48:04 +0900

    2021 01 27
    uddate

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("potato"));
+// <App /> : component라고 부름. HTML을 반환하는 함수!
+// react는 component와 함께 동작함, component는 대문자로 시작
